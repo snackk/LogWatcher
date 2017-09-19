0fd7a9acc88950b65ae8c54e54e9bf2c0c305550
snackk <santos_matrix@sapo.pt>
2017-09-20 00:02:17 +0100

    Fixed logswatcher

--- LogsWatcher.sh
+++ LogsWatcher.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+COMMAND="$(less /var/log/auth.log | grep "Accepted password\|Accepted publickey" | tail -1)"
+echo ${COMMAND} > /tmp/log
+COMMAND=""
 while true; do
     inotifywait -e modify /var/log/auth.log
-    COMMAND="$(less /var/log/auth.log | grep "Accepted password\|Accepted publickey" | tail -5)"
-    echo ${COMMAND}
-    python $HOME/git/logwatcher/ReportActivity.py "${COMMAND}"
-   COMMAND=""
+    COMMAND="$(less /var/log/auth.log | grep "Accepted password\|Accepted publickey" | tail -1)"
+    echo ${COMMAND} > /tmp/result
+    DIFF="$(diff --changed-group-format='%<' --unchanged-group-format='' /tmp/result /tmp/log)"
+
+    if [ "$DIFF" != "" ]; then
+        echo ${COMMAND} > /tmp/log
+        #echo ${DIFF} >> /home/pi/lol
+        python /home/pi/git/logwatcher/ReportActivity.py "${DIFF}"
+    fi
+    COMMAND=""
+    DIFF=""
 done
+
