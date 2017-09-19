719960144b61d7c2ac2d170a3229d69a063af5f4
snackk <santos_matrix@sapo.pt>
2017-09-19 22:56:52 +0100

    Changed path

--- LogsWatcher.sh
+++ LogsWatcher.sh
@@ -3,6 +3,6 @@ while true; do
     inotifywait -e modify /var/log/auth.log
     COMMAND="$(less /var/log/auth.log | grep "Accepted password\|Accepted publickey" | tail -5)"
     echo ${COMMAND}
-    python /home/pi/Downloads/ReportActivity.py "${COMMAND}"
+    python $HOME/git/logwatcher/ReportActivity.py "${COMMAND}"
    COMMAND=""
 done
