60a8cbb64ae2a4741cc9c18a1dedb50ee9f3a2b9
Diogo Santos <snackk@users.noreply.github.com>
2019-01-03 10:34:31 +0000

    password

--- ReportActivity.py
+++ ReportActivity.py
@@ -8,7 +8,7 @@ def main(argv):
     email = "owncloud.watcher@gmail.com"#os.environ["log_email_from"]
     emailTo = "snackk026@gmail.com"#os.environ["log_email_to"]
     subject = "Owncloud - New SSH session created"
-    passwd = "emc2changedall"#os.environ["log_passwd"]
+    passwd = "password"#os.environ["log_passwd"]
     
     if message:
     	report = GmailReporter(email, emailTo, subject, passwd, message)
